c90a3aeb61a7ef96cbbec87465f01394cb904129
Spark <sparkismad@gmail.com>
2024-11-01 11:33:21 +0100

    first commit

---
+++ SQLiteConductor/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SQLiteConductor;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ SQLiteConductor/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SQLiteConductor;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
